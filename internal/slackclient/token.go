feeeede65973b0cc88a0a6abfefb8bb860ae601c
Sunny Days <dirtymula86@gmail.com>
2024-01-11 12:23:02 -0600

    Initialize macos

---
+++ internal/slackclient/token.go
@@ -0,0 +1,165 @@
+package slackclient
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha1"
+	"database/sql"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+
+	"golang.org/x/crypto/pbkdf2"
+
+	_ "modernc.org/sqlite"
+)
+
+type SlackAuth struct {
+	Token   string
+	Cookies map[string]string
+}
+
+var stmt = "SELECT value, encrypted_value FROM cookies WHERE host_key=\".slack.com\" AND name=\"d\""
+
+type CookieDecryptor interface {
+	Password() string
+}
+
+func getCookie() (string, error) {
+	var cookieDBFile string
+
+	for _, config := range slackConfigDirs() {
+		cookieDBFile = path.Join(config, "Slack", "Cookies")
+		stat, err := os.Stat(cookieDBFile)
+		if errors.Is(err, fs.ErrNotExist) {
+			continue
+		}
+		if err != nil {
+			return "", fmt.Errorf("could not access Slack cookie database: %w", err)
+		}
+		if stat.IsDir() {
+			return "", fmt.Errorf("directory found at expected Slack cookie database location %q", cookieDBFile)
+		}
+		break
+	}
+
+	if cookieDBFile == "" {
+		return "", errors.New("no Slack cookie database found. Are you definitely logged in?")
+	}
+
+	db, err := sql.Open("sqlite", cookieDBFile)
+	if err != nil {
+		return "", err
+	}
+
+	var cookie string
+	var encrypted_value []byte
+	err = db.QueryRow(stmt).Scan(&cookie, &encrypted_value)
+	if err != nil {
+		return "", err
+	}
+
+	if cookie != "" {
+		return cookie, nil
+	}
+
+	// We need to decrypt the cookie.
+
+	key, err := cookiePassword()
+	if err != nil {
+		return "", fmt.Errorf("failed to get cookie password: %w", err)
+	}
+	dk := pbkdf2.Key(key, []byte("saltysalt"), iterations(), 16, sha1.New)
+
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		return "", err
+	}
+
+	iv := make([]byte, 16)
+	for i := range iv {
+		iv[i] = ' '
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+
+	encrypted_value = encrypted_value[3:]
+	mode.CryptBlocks(encrypted_value, encrypted_value)
+
+	bytesToStrip := int(encrypted_value[len(encrypted_value)-1])
+
+	return string(encrypted_value[:len(encrypted_value)-bytesToStrip]), nil
+}
+
+var apiTokenRE = regexp.MustCompile("\"api_token\":\"([^\"]+)\"")
+
+func getSlackAuth(team string) (*SlackAuth, error) {
+	cookie, err := getCookie()
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := http.NewRequest("GET", fmt.Sprintf("https://%s.slack.com", team), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	r.AddCookie(&http.Cookie{Name: "d", Value: cookie})
+
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("status code %d", resp.StatusCode)
+	}
+
+	bs, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	matches := apiTokenRE.FindSubmatch(bs)
+	if matches == nil {
+		return nil, errors.New("api token not found")
+	}
+
+	return &SlackAuth{Token: string(matches[1]), Cookies: map[string]string{"d": cookie}}, nil
+}
+
+var errorNoSlackAuth = errors.New("environment variable SLACK_AUTH not in expected format. Try cloning https://github.com/chrisgavin/slacktoken and running `export SLACK_AUTH=\"$(python3 -m slacktoken get --workspace github)\"`")
+
+func getSlackAuthFromEnv() (*SlackAuth, error) {
+	slackAuth := os.Getenv("SLACK_AUTH")
+	if slackAuth == "" {
+		return nil, errorNoSlackAuth
+	}
+
+	token, cookie, found := strings.Cut(slackAuth, "\n")
+	if !found {
+		return nil, errorNoSlackAuth
+	}
+
+	key, value, found := strings.Cut(cookie, "=")
+	if !found {
+		return nil, errorNoSlackAuth
+	}
+
+	cookie, err := url.PathUnescape(value)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unescape cookie value: %w", err)
+	}
+
+	return &SlackAuth{
+		Token:   token,
+		Cookies: map[string]string{key: cookie},
+	}, nil
+}
